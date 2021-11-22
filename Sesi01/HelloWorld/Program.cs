8262ca29003cbcc41e36a596eff6af776d9207be
gca98 <giovannichristian98@gmail.com>
2021-11-22 13:52:08 +0700

    init

--- Sesi01/HelloWorld/Program.cs
+++ Sesi01/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Nama saya Giovanni Christian");
+            Console.WriteLine("Nama saya Giovanni");
         }
     }
 }
